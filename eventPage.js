3f9eb8421e3a4e5919fec35dd0e2dedc9a6c889a
noname01 <hjc.noname@gmail.com>
2014-09-08 23:19:25 -0700

    fix new tab and switching tab

--- eventPage.js
+++ eventPage.js
@@ -4,10 +4,15 @@ chrome.runtime.onMessage.addListener(function(data, sender) {
     chrome.tabs.query({url: regUrl}, function(resultTabs) {
         if(resultTabs.length  == 0){
             chrome.tabs.create({url: regUrl, active: true}, function (tab) {
-                chrome.tabs.sendMessage(tab.id, {toadd: data.toadd});
+                chrome.tabs.onUpdated.addListener(function(tabId, info) {
+                    if (info.status == "complete" && tabId == tab.id) {
+                        chrome.tabs.sendMessage(tab.id, {toadd: data.toadd});
+                    }
+                });
             })
         } else {
             chrome.tabs.sendMessage(resultTabs[0].id, {toadd: data.toadd});
+            chrome.tabs.update(resultTabs[0].id, {active: true});
         }
     });
 });
