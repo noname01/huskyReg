358e453ca6e84ab235f2e1212485a425abdddcfe
noname01 <hjc.noname@gmail.com>
2014-09-15 14:23:56 -0700

    prep for omnibox reg

--- eventPage.js
+++ eventPage.js
@@ -1,4 +1,5 @@
-//todo: better css; add to schFinder; add btns to myPlan; omnibox reg
+//todo: connect with api server
+//todo: add btns to myPlan; omnibox goto/reg
 
 var regUrl = "https://sdb.admin.washington.edu/students/uwnetid/register.asp";
 
